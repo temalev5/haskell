0df4362d46626e5926becffd88e9239db9d76554
temalev5 <49922049+temalev5@users.noreply.github.com>
2019-06-11 11:06:59 +0300

    Delete thlab.hs

--- thlab.hs
+++
@@ -1,6 +0,0 @@
-ft = ["DOG","CAT","FOX"]
-sec = ["RET","GET","PUT","OUT","IN"]
-th = ["MOV","ADD","MUL","DEV"]
-concathead x y z = head x: head y: head z:[]
-concatelemen x y z = x !! 2: y !! 3: z !! 2:[]
-discremenant a b c = (b*b)-(4*a*c)
